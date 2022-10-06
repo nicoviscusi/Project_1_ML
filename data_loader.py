a4cabbf64b8ed803deb559ff067614bc275a1bc7
Francesco Sala <francesco.sala@epfl.ch>
2022-10-06 20:53:56 +0200

    correction of typos

--- data_loader.py
+++ data_loader.py
@@ -3,7 +3,7 @@ def load_training_data():
     path_dataset = "train.csv"
     all_data = np.genfromtxt(path_dataset, delimiter=",", skip_header=1, usecols=[0, 31])
     ids = all_data[0,:]
-    prepredicions = all_data[1,:]
+    predictions = all_data[1,:]
     data = all_data[2:,:]
     return ids, predictions, data
 
