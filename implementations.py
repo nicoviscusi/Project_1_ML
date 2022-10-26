17821370a43e426b85f451732b092aac5fca676c
TitBro <94957095+TitBro@users.noreply.github.com>
2022-10-26 15:21:22 +0200

    modifs

--- implementations.py
+++ implementations.py
@@ -352,11 +352,11 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     """
 
     w = initial_w
-    loss = compute_log_loss(y, tx, w)
     for n_iter in range(max_iters):
         gradient = compute_ridge_log_gradient(y, tx, w, lambda_)
         w = w - gamma * gradient
-        loss = compute_log_loss(y, tx, w)
+        loss = compute_ridge_log_loss(y, tx, w)
+    loss = compute_log_loss(y, tx, w)
     return w, loss
 
 
